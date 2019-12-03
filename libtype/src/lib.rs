eddb1a770c670b35ac9c6c061606f2732d596f2e
eomain <eomain@protonmail.com>
2019-12-03 01:36:21 +0000

    type check for 'id' and 'expr'

--- libtype/src/lib.rs
+++ libtype/src/lib.rs
@@ -1,7 +1,44 @@
+
+extern crate libsym;
+extern crate libast;
+
+use libsym::Scope;
+use libsym::Table;
+use libsym::TypeInfo;
+use libast::Variable;
+
+#[derive(Debug)]
+pub enum Error {
+    Id
+}
+
+#[derive(Debug)]
+pub struct Env<'a> {
+    table: &'a Table<'a>
+}
+
+mod check {
+    use super::Error;
+    use libsym::Scope;
+    use libsym::TypeInfo;
+    use libast::Expr;
+
+    fn id(scope: &Scope, name: &str) -> Result<TypeInfo, Error>
+    {
+        if let Some(info) = scope.find(name) {
+	    Ok(info)
+        } else {
+	    Err(Error::Id)
+	}
+    }
+
+    fn expr(scope: &Scope, expr: Expr) -> Result<TypeInfo, Error>
+    {
+        unimplemented!()
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+   
 }
