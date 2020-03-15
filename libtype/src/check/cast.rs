56a60f85210d29db2af2433447479283cc4b7e86
eomain <eomain@protonmail.com>
2020-03-15 20:51:32 +0000

    use derived type and add unit test

--- libtype/src/check/cast.rs
+++ libtype/src/check/cast.rs
@@ -4,6 +4,7 @@ use libast::Cast;
 
 fn castable(a: &DataType, b: &DataType) -> bool
 {
+    let (a, b) = (a.derived(), b.derived());
     if (a == b) {
         return true;
     }
@@ -26,10 +27,28 @@ pub fn cast(i: &mut Info, s: &mut Scope,
             c: &mut Cast, expt: Option<DataType>) -> Result<(), Error>
 {
     expr(i, s, &mut *c.expr, None)?;
-    let (a, b) = (&c.dtype, c.expr.get_type());
+    let (a, b) = (named(s, &c.dtype), c.expr.get_type());
     if !castable(a, b) {
         Err(error!("cannot cast type from `{}` into `{}`", b, a))
     } else {
+        c.dtype = a.clone();
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use std::rc::Rc;
+    use super::*;
+    use libast::Unique;
+    use DataType::*;
+    use IntType::*;
+
+    #[test]
+    fn cast_test()
+    {
+        let unique = Unique::new("Object", Integer(S32));
+        assert_eq!(castable(&unique.into(), &Integer(S32)), true);
     }
 }
