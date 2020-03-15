265eab5862805c8027be0919eacc072a5a69ffca
eomain <eomain@protonmail.com>
2020-03-15 01:52:18 +0000

    require meta data for type checker

--- libtype/src/lib.rs
+++ libtype/src/lib.rs
@@ -15,6 +15,22 @@ use libast::SyntaxTree;
 
 pub use error::Error;
 
+// Meta data to be used by the
+// type checker
+pub struct TypeMeta {
+    // if the main function is required
+    main: bool
+}
+
+impl TypeMeta {
+    pub fn new(main: bool) -> Self
+    {
+        Self {
+            main
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Env<'a> {
     pub table: Table<'a>
@@ -29,43 +45,37 @@ impl<'a> Env<'a> {
     }
 }
 
-/*mod efmt {
-    use libast::DataType;
-
-    pub fn args(args: &Vec<DataType>) -> String
-    {
-        let mut s = String::new();
-        for i in 1..args.len() {
-            s.push();
-        }
-        s
+fn require_main(meta: &TypeMeta, global: &Table) -> Result<(), Error>
+{
+    if meta.main && !global.has_main() {
+        return Err(Error::Custom("function 'main' undefined".into()));
     }
-}*/
+    Ok(())
+}
 
-pub fn init(ast: &mut SyntaxTree) -> Result<(), Error>
+pub fn init(meta: TypeMeta, ast: &mut SyntaxTree) -> Result<(), Error>
 {
     let mut env = Env::new();
+    let global = &mut env.table;
 
     for d in &ast.declarations {
         let args = d.param.clone();
         let ret = d.ret.clone();
-        env.table.insert(&d.name, (TypeInfo::Function((args, ret)), true));
+        global.insert(&d.name, (TypeInfo::Function((args, ret)), true));
     }
 
     for f in &ast.functions {
         let args = Vec::from(&f.param).iter().map(|a| a.1.clone()).collect();
         let ret = f.ret.clone();
-        env.table.insert(&f.name, (TypeInfo::Function((args, ret)), true));
+        global.insert(&f.name, (TypeInfo::Function((args, ret)), true));
     }
 
     for f in &mut ast.functions {
-        let mut s = env.table.scope();
+        let mut s = global.scope();
         check::fpass(&mut s, f)?;
     }
 
-    if !env.table.has_main() {
-        return Err(Error::Custom("function 'main' undefined".into()));
-    }
+    require_main(&meta, &global)?;
 
     Ok(())
 }
