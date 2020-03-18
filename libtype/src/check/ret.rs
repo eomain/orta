bd0e54394a2ce359f3fd0ff6620fe74680b0e954
eomain <eomain@protonmail.com>
2020-03-18 01:53:24 +0000

    type check method return

--- libtype/src/check/ret.rs
+++ libtype/src/check/ret.rs
@@ -1,6 +1,7 @@
 
 use super::*;
 use libast::ExprList;
+use libast::Method;
 
 #[inline]
 fn expr(i: &mut Info, s: &mut Scope, e: &Expr) -> bool
@@ -35,19 +36,28 @@ fn cond(i: &mut Info, s: &mut Scope, br: &IfExpr) -> bool
     }
 }
 
-pub fn returns(i: &mut Info, s: &mut Scope, f: &Function) -> bool
+fn last_expr(i: &mut Info, s: &mut Scope, e: &ExprList) -> bool
 {
     if i.ret == DataType::Unit {
         return true;
     }
 
-    if let Some(expr) = f.expr.last() {
+    if let Some(expr) = e.last() {
         return match expr {
             Expr::Return(_) => true,
             Expr::If(br) => cond(i, s, br),
             _ => false
         };
     }
-
     false
+}
+
+pub fn returns(i: &mut Info, s: &mut Scope, f: &Function) -> bool
+{
+    last_expr(i, s, &f.expr)
+}
+
+pub fn method_returns(i: &mut Info, s: &mut Scope, m: &Method) -> bool
+{
+    last_expr(i, s, &m.expr)
 }
